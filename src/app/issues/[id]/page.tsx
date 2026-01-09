f5f78aaa232cea24d35d7ad5cfb1037a07318d6a
markus90souza <markus90souza@gmail.com>
2026-01-09 12:42:35 -0300

    fix

--- src/app/issues/[id]/page.tsx
+++ src/app/issues/[id]/page.tsx
@@ -1,15 +1,6 @@
 import Link from 'next/link'
 import type { Metadata } from 'next'
-import { IssueCommentsList } from './_components/issue-comments-list'
-import { IssueCommentsListSkeleton } from './_components/issue-comments-list-skeleton'
-import { Suspense } from 'react'
-import { IssueLikeButton } from './_components/issue-like-button'
-import { IssueCommentForm } from './_components/issue-comment-form'
-import { authClient } from '@/lib/auth-client'
-import { headers } from 'next/headers'
-import { createComment } from '@/http/create-comment'
-
-import { MoveLeftIcon, ArchiveIcon } from 'lucide-react'
+import { MoveLeftIcon } from 'lucide-react'
 import { getIssue } from '@/http/get-issue'
 import { IssueDetails } from './_components/issue-details'
 interface IssuePageProps {
@@ -28,17 +19,9 @@ export const generateMetadata = async ({
   }
 }
 
-const statusLabels = {
-  backlog: 'Backlog',
-  todo: 'To Do',
-  in_progress: 'In Progress',
-  done: 'Done',
-} as const
-
 export default async function IssuePage({ params }: IssuePageProps) {
   const { id } = await params
 
- 
   return (
     <main className="max-w-225 mx-auto w-full flex flex-col gap-4 p-6 bg-navy-800 border-[0.5px] border-navy-500 rounded-xl">
       <Link
@@ -50,8 +33,6 @@ export default async function IssuePage({ params }: IssuePageProps) {
       </Link>
 
       <IssueDetails issueId={id} />
-
-
     </main>
   )
 }
